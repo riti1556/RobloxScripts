ff4cf992dc8d44b2a2601753bd7d36018816c505
riti1556 <andreraku2011@gmail.com>
2025-04-27 18:57:48 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local scripts = {
     ["BladeballAutoParry"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/BladeballAutoParry.lua",
-    ["SimpleAutoParry"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/SimpleAutoParry.lua",
+    ["WorkingAutoParry"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/WorkingAutoParry.lua",
     ["AutoJump"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/AutoJump.lua",
     ["WalkSpeed"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/WalkSpeed.lua",
     ["ESP"] = "https://raw.githubusercontent.com/riti1556/RobloxScripts/main/ESP.lua"
